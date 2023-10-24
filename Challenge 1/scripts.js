8faa6f37653de107b71ad0224a1174ba0caf097e
Liezl Mouton <liz.mouton1@gmail.com>
2023-10-24 16:06:17 +0200

    Solved IWA14 Challenge 1

--- Challenge 1/scripts.js
+++ Challenge 1/scripts.js
@@ -1,14 +1,43 @@
-firstName = "John";
-age = 35;
-hobby = "Coding";
+// SIMPLE ANSWER
 
-const logTwice = () => {
-  console(parameter);
-  console(parameter);
+// Initialized the variables as const
+const firstName = "John";
+const age = 35;
+const hobby = "Coding";
+
+/*  1. Changed hobby function declaration into the below arrow function (the hoisting was causing a bug)
+    2. Changed from name to firstName to call it correctly (inside the interpolation)
+    3. Moved it to the top. Logically you create the sentence first and then log it so you know what you are logging
+     */
+const logSentence = () => {
+  console.log(`Hello, ${firstName} (${age}). I love ${hobby}!`); //
 };
 
-function hobby() {
-  logTwice(`Hello, ${name} (${age}). I love ${hobby}!`);
+const logSentenceTwice = () => {
+  logSentence();
+  logSentence();
+}; // Simply calls logSentence twice. Why does something like logSentence() * 2 not work?
+
+logSentenceTwice(); // Calls logTwice()
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+// MORE ADVANCED ANSWER USING PARAMETERS
+
+// Initialized the variables as const
+const firstName = "John";
+const age = 35;
+const hobby = "Coding";
+
+// Fixed console.log syntax and gave the function a parameter
+const logTwice = (thisIsAParameter) => {
+  console.log(thisIsAParameter);
+  console.log(thisIsAParameter);
+};
+
+// Left this as a traditional function but changed the name so that the hoisting wouldn't cause a bug
+function logHobby() {
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`); // Changed name to firstName
 }
 
-hobby();
+logHobby(); // Logs the desired result
